6042fa2d660ac77899c4aebad3efdc7dcc9f455a
unknown <amara.mohammed@outlook.com>
2023-07-05 16:23:20 +0100

    sortie

--- src/views/project/ProjectDashboard/sortie.js
+++ src/views/project/ProjectDashboard/sortie.js
@@ -4,8 +4,8 @@ export default function () {
     return (
 
         <div className="grid grid-flow-row auto-rows-max gap-4 ">
-            <DemoBoxContent className="shadow-lg bg-sky-100 text-gray-500">
-                <p className="text-gray-500 text-center bg-blue-200 rounded">VANT LA CHIRURGIE</p>
+            <DemoBoxContent className="shadow-lg bg-sky-500 text-white-500">
+                <p className="text-white-500 text-xl text-center bg-blue-200 rounded">AVANT LA CHIRURGIE</p>
 
                 Madame, Monsieur,
 
@@ -18,35 +18,43 @@ export default function () {
 
                 L’équipe de chirurgie cardiaque
             </DemoBoxContent>
-            <DemoBoxContent className="text-gray-500 shadow-lg bg-sky-300">
-                <p className="text-center bg-blue-200 rounded"> EN RÉSUMÉ : CONSEILS ET CONSIGNES POST OPÉRATOIRES
+            <DemoBoxContent className="text-white-500 shadow-lg bg-sky-500">
+                <p className="text-center text-xl bg-blue-200 rounded">  CONSEILS ET CONSIGNES POST OPÉRATOIRES
                 </p>
 
-<div className="text-justify">
-    <p> 🔵 Arrêter de fumer</p>
-    <p> 🔵 Dormir sur le dos pendant 1 mois</p>
-    <p> 🔵 Ne pas porter de poids pendant 2 mois</p>
-    <p> 🔵 Marcher tous les jours, 2 fois par jour pendant 30min à 1h
-    </p>
-    <p> 🔵 Conseils d’hygiène corporelle :
-    </p></div>
-
+                <div className="text-justify">
+                    <p> 🔵 Arrêter de fumer</p>
+                    <p> 🔵 Dormir sur le dos pendant 1 mois</p>
+                    <p> 🔵 Ne pas porter de poids pendant 2 mois</p>
+                    <p> 🔵 Marcher tous les jours, 2 fois par jour pendant 30min à 1h
+                    </p>
+                    <p> 🔵 Conseils d’hygiène corporelle :
+                    </p></div>
 
 
             </DemoBoxContent>
-            <DemoBoxContent className=" text-gray-500 shadow-lg bg-sky-500">
-                <p className="text-center bg-blue-200 rounded">MON SÉJOUR AVANT LA CHIRURGIE</p>
+            <DemoBoxContent className=" text-justify text-white-500 shadow-lg bg-sky-500">
+                <p className="text-xl text-center bg-blue-200 rounded">CICATRICE - STERNUM</p>
 
-                Madame, Monsieur,
+                <p className="text-xl text-center"> Le sternum met 6 à 8 semaines pour être consolidé.</p>
 
-                Vous venez de vous faire opérer du coeur dans notre service. Nous espérons que votre séjour s’est bien
-                passé. Nous tenons à vous fournir quelques conseils pour un retour à domicile dans les meilleures
-                conditions.
+                <p> - Mettre la ceinture de contention jour et nuit pour les hommes pendant 1 mois
+                </p>
+                <p> - Mettre la ceinture de contention jour et nuit pour les hommes pendant 1 mois
+                </p>
+                <p> - Mettre le soutien-gorge jour et nuit pour les femmes pendant 1 mois
+                </p>
+            </DemoBoxContent>
+            <DemoBoxContent className=" text-justify text-white-500 shadow-lg bg-sky-500">
+                <p className="text-xl text-center bg-blue-200 rounded">SURVEILLANCE</p>
 
-                Lisez attentivement les informations contenues sur cette page avant votre départ et n’hésitez pas à
-                poser des questions à l’équipe soignante si nécessaire.
+                <p className="text-red-500 text-xl text-center"> Lors de votre retour à domicile :</p>
 
-                L’équipe de chirurgie cardiaque
+                <p> - Contactez votre médecin traitant pour mettre en place un suivi régulier (tous les 2 à 3 mois)</p>
+                <p> - Contactez votre cardiologue pour prendre RDV à 1 mois après la chirurgie
+                </p>
+                <p> - Prenez bien le traitement prescrit
+                </p>
             </DemoBoxContent>
         </div>
 
