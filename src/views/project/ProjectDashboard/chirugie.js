fa1defe405b724e1181282691ba1b559dd26ced5
unknown <amara.mohammed@outlook.com>
2023-07-05 14:52:42 +0100

    add_theme

--- src/views/project/ProjectDashboard/chirugie.js
+++ src/views/project/ProjectDashboard/chirugie.js
@@ -1,8 +1,9 @@
 import React from "react";
+import DemoBoxContent from "../../../components/docs/DemoBoxContent";
 
 export default function Chirugie() {
     return (
-        <div className="grid grid-rows-4 grid-flow-col gap-4 text-justify">
+      /*  <div className="grid grid-rows-4 grid-flow-col gap-4 text-justify">
             <div>
                 <div className="bg-white rounded ">
                     <div className="h-2"></div>
@@ -205,6 +206,209 @@ export default function Chirugie() {
             </div>
 
 
+        </div>*/
+        <div className="grid grid-flow-row auto-rows-max gap-4 ">
+            <DemoBoxContent className="shadow-lg bg-sky-100 text-gray-500">
+                <p className="text-gray-500 text-center bg-blue-200 rounded">coordonnées</p>
+
+                <div>
+                    <div className="bg-white rounded ">
+                        <div className="h-2"></div>
+                        <div className="flex">
+                            <div className="w-3"></div>
+                            <div className="w-93">
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-lg font-bold text-left text-blue-500 ">📍 Adresse</p>
+                                    </div>
+                                </div>
+                                <div className="h-2"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify ">Service de chirurgie cardio-vasculaire</p>
+                                    </div>
+                                </div>
+                                <div className="h-1"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">Akdital Tanger | Hôpital Privé de Tanger</p>
+                                    </div>
+                                </div>
+                                <div className="h-1"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">Av. Moulay Rachid, Tanger 90050</p>
+                                    </div>
+                                </div>
+                                <div className="h-2"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <a href="https://goo.gl/maps/aDi6Ws3qa1K68Kwu8" target="_blank"
+                                           rel="noopener noreferrer">
+                                            <div className="flex items-center">
+                                                <img className="w-6 h-6" src="" alt=""/>
+                                                <p className="text-lg font-bold text-left text-blue-500">Afficher l'adresse
+                                                    dans Google Maps</p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="h-2"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-lg font-bold text-justify text-blue-500">🚘 Comment s'y rendre
+                                            ?</p>
+                                    </div>
+                                </div>
+                                <div className="h-2"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">En voiture personnelle;</p>
+                                    </div>
+                                </div>
+                                <div className="h-1"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">- Parking P4, avenue Jean Poulhès, sur la droite puis
+                                            accès au site par ascenseur</p>
+                                    </div>
+                                </div>
+                                <div className="h-1"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">- Parkings P1 et P2, dédiés aux patients venant en
+                                            consultation</p>
+                                    </div>
+                                </div>
+                                <div className="h-1"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">- « Dépose-minute » devant le hall d’accueil puis
+                                            parkings P1 ou P2</p>
+                                    </div>
+                                </div>
+                                <div className="h-2"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">En ambulance ou véhicule sanitaire (VSL) :</p>
+                                    </div>
+                                </div>
+                                <div className="h-1"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">Accès par la route de Narbonne, prendre la 1ère entrée à
+                                            gauche après le pont du périphérique (avant le giratoire)</p>
+                                    </div>
+                                </div>
+                                <div className="h-2"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">En métro et bus :</p>
+                                    </div>
+                                </div>
+                                <div className="h-1"></div>
+                                <div className="flex">
+                                    <div className="w-full">
+                                        <p className="text-justify">- Métro ligne B, arrêt "Faculté de Pharmacie" puis bus
+                                            n° 37 (arrêt Rangueil-Maternité)</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </DemoBoxContent>
+            <DemoBoxContent className="text-gray-500 shadow-lg bg-sky-300">
+                <p className="text-center bg-blue-200 rounded"></p>
+
+                Le jour de votre arrivée
+
+                L’équipe paramédicale vous accueille et vous serez examiné(e) par le médecin du service.
+
+                Pensez à signaler vos régimes alimentaires, allergies et convictions religieuses ou toute spécifiée à
+                l’aide-soignante. Votre alimentation, au cours de l’hospitalisation, est ainsi adaptée grâce au
+                partenariat avec le service restauration.
+            </DemoBoxContent>
+            <DemoBoxContent className=" text-gray-500 shadow-lg bg-sky-500">
+                <div className="bg-white rounded ">
+                <div>
+                <p className="text-center bg-blue-200 rounded"></p>
+
+                <div className="flex">
+                    <p className="text-lg font-bold text-center text-blue-500">NUMÉROS DE TÉLÉPHONE UTILES</p>
+                </div>
+        </div>
+    <div className="h-2"></div>
+
+    <div className="flex">
+        <p className="text-base font-bold text-left text-blue-500">☎️ CONSULTATION - PROGRAMMATION
+            PRÉPARATION PRÉOPÉRATOIRE</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">Du lundi au vendredi de 8h30 à 16h</p>
+    </div>
+    <div className="h-1.5"></div>
+    <div className="flex">
+        <p className="text-base font-bold text-left text-blue-500">Marie - Elisabeth - Sandrine -
+            Agnès</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; Prise de rendez-vous : 05 61 32 26 55</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; Programmation : 05 61 32 38 76</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; Infirmières de consultation : 05 61 32 35 53</p>
+    </div>
+    <div className="h-1.5"></div>
+    <div className="flex">
+        <p className="text-base font-bold text-left text-blue-500">☎️ SECRÉTARIAT</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">Du lundi au vendredi de 8h30 à 17h30</p>
+    </div>
+    <div className="h-1.5"></div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; 05 61 32 26 52</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; 05 61 32 26 53</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; 05 61 32 26 54</p>
+    </div>
+    <div className="h-1.5"></div>
+    <div className="flex">
+        <p className="text-base font-bold text-left text-blue-500">☎️ CCV 51 (unité 1)</p>
+    </div>
+    <div className="h-1.5"></div>
+    <div className="flex">
+        <p className="text-base font-bold text-left text-blue-500">Cadre infirmier - Mme C. Falco : 05
+            61 32 24 36</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; Accueil : 05 61 32 26 57</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; Salle de soins : 05 61 32 26 56</p>
+    </div>
+    <div className="h-1.5"></div>
+    <div className="flex">
+        <p className="text-base font-bold text-left text-blue-500">☎️ CCV 51 (unité 2)</p>
+    </div>
+    <div className="h-1.5"></div>
+    <div className="flex">
+        <p className="text-base font-bold text-left text-blue-500">Cadre infirmier - Mme C. Faure : 05
+            61 32 24 47</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; Accueil : 05 61 32 26 59</p>
+    </div>
+    <div className="flex">
+        <p className="text-base text-left">&gt; Salle de soins : 05 61 32 26 58</p>
+    </div></div>
+            </DemoBoxContent>
         </div>
     )
 }
